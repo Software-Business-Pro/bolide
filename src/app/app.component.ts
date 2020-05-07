30fc6bb49c3c03c4e1d31747aee634182054f96b
ClementLegouest <clement.legouest@gmail.com>
2020-05-07 16:02:03 +0200

    auth system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'page-perso';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAhXHNspAM0-gKeb10RplyFComwlIGHVBQ",
+      authDomain: "page-perso-92c2e.firebaseapp.com",
+      databaseURL: "https://page-perso-92c2e.firebaseio.com",
+      projectId: "page-perso-92c2e",
+      storageBucket: "page-perso-92c2e.appspot.com",
+      messagingSenderId: "765812661984",
+      appId: "1:765812661984:web:7608b4f1c2ab2c68cdfa85",
+      measurementId: "G-6WCEE8T007"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
